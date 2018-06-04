b575c1ee6820b041cb44d69987616eff445984e6
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-06-04 12:49:34 +0200

    Now really added Workflow editor drawer for inspector :)

---
+++ VOCASY/VOCASY.Editor/WorkflowDrawer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using VOCASY.Common;
+namespace VOCASY.Editor
+{
+    /// <summary>
+    /// Class that modifies Workflow inspector view
+    /// </summary>
+    [CustomEditor(typeof(Workflow))]
+    public class WorkflowsDrawer : UnityEditor.Editor
+    {
+        Workflow workflow;
+        private void OnEnable()
+        {
+            workflow = target as Workflow;
+        }
+        /// <summary>
+        /// Method that modifies VoiceChatSettings inspector view
+        /// </summary>
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            using (new GUILayout.HorizontalScope())
+            {
+                if (GUILayout.Button("Save Players Mute Status", EditorStyles.miniButton))
+                {
+                    if (EditorUtility.DisplayDialog("Save", "Saved mute statuses will be overwritten by current statuses", "Save", "Cancel"))
+                        workflow.SaveCurrentMuteStatuses();
+                }
+
+                else if (GUILayout.Button("Load Saved Players Mute Status", EditorStyles.miniButton))
+                {
+                    if (EditorUtility.DisplayDialog("Load", "By loading saved values the unsaved statuses may be lost", "Load", "Cancel"))
+                        workflow.LoadSavedMuteStatuses();
+                }
+            }
+        }
+    }
+}
